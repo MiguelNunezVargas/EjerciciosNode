b5e0639ac576bd0d643e0de453a7afbdbb21af3d
Miguel <miguelnunezvargas@gmail.com>
2019-07-28 01:06:21 -0400

    Cambios variable uri mongo

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://hioks:35Upiaz8DQ8fuAUD@cluster0-p1lhf.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 
